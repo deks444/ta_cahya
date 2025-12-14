e2ce37b3f38fb6bf304c81b1da7da1b9a1e76561
deks444 <ardijuniawan698@gmail.com>
2025-12-14 14:33:08 +0800

    testing session

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Middleware\AddSecurityHeaders;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageController::class, 'index'])->name('main');
-Route::get('/peraturan', [PageController::class, 'rules'])->name('peraturan');
-Route::get('/event/sertifikat', [PageController::class, 'sertifikat'])->name('event.sertifikat');
-Route::get('/event/beritaacara', [PageController::class, 'acara'])->name('event.acara');
-Route::get('/aboutus', [PageController::class, 'about'])->name('about');
-Route::get('/faq', [PageController::class, 'faq'])->name('faq');
+// Rute login tanpa middleware
 Route::get('/login', [PageController::class, 'login'])->name('login');
+
+// Grup rute yang memakai middleware AddSecurityHeaders
+Route::middleware(AddSecurityHeaders::class)->group(function () {
+    Route::get('/', [PageController::class, 'index'])->name('main');
+    Route::get('/peraturan', [PageController::class, 'rules'])->name('peraturan');
+    Route::get('/event/sertifikat', [PageController::class, 'sertifikat'])->name('event.sertifikat');
+    Route::get('/event/beritaacara', [PageController::class, 'acara'])->name('event.acara');
+    Route::get('/aboutus', [PageController::class, 'about'])->name('about');
+    Route::get('/faq', [PageController::class, 'faq'])->name('faq');
+});
